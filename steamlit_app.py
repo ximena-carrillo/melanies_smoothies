eeb0370a1e865a05fbe5fb5a87ccdcc27539e25b
ximena-carrillo <ximena_carrillo@trimble.com>
2025-07-09 09:22:58 -0700

    Update steamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+
+cnx = st.connection("snowflake")
+session = cnx.session() 
 
 # Write directly to the app
 st.title(f"Customize your smoothie! :cup_with_straw:")
