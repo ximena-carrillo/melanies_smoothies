4013afd8348cb171a5449dcde40156c7e5c0a51e
ximena-carrillo <ximena_carrillo@trimble.com>
2025-07-09 11:26:23 -0700

    fruit

--- steamlit_app.py
+++ steamlit_app.py
@@ -32,7 +32,7 @@ if ingredient_list and name_on_order:
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = f"""
