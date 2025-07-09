571067e3f30953e6f314c48d4a105deea154ada5
ximena-carrillo <ximena_carrillo@trimble.com>
2025-07-09 11:16:45 -0700

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,6 +2,10 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 cnx = st.connection("snowflake")
 session = cnx.session() 
 
