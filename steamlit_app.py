71202549be6927b1e4aabc6c73b28903e385020f
ximena-carrillo <ximena_carrillo@trimble.com>
2025-07-09 12:22:44 -0700

    SEARCH ON

--- steamlit_app.py
+++ steamlit_app.py
@@ -15,7 +15,7 @@ st.write(
 )
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 pd_df = my_dataframe.to_pandas()
@@ -45,7 +45,7 @@ if ingredient_list and name_on_order:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
-        
+
     st.write(ingredients_string)
     my_insert_stmt = f"""
         INSERT INTO smoothies.public.orders(ingredients, name_on_order)
