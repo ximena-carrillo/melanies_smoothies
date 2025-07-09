a5ce0b9a89b671159c2003d0a8496aef58c81671
ximena-carrillo <ximena_carrillo@trimble.com>
2025-07-09 11:19:47 -0700

    json

--- steamlit_app.py
+++ steamlit_app.py
@@ -4,7 +4,8 @@ from snowflake.snowpark.functions import col
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+
 
 cnx = st.connection("snowflake")
 session = cnx.session() 
