52b23f04b9cb73187368e310ebdd0eaab4f595d0
ximena-carrillo <ximena_carrillo@trimble.com>
2025-07-09 11:22:09 -0700

    table api

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,11 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-
-
 cnx = st.connection("snowflake")
 session = cnx.session() 
 
@@ -45,3 +40,9 @@ if ingredient_list and name_on_order:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
